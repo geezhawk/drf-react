ab92691994024588a0f326fa79de81d35f6d3925
Abe Park <abe.park@hulu.com>
2017-05-02 12:35:49 -0700

    add books to admin

--- api/models.py
+++ api/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200, unique=True)
     author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title + " - by " + self.author
